c1f1f8bef8a545e70921396e4012fcc71255417b
pax217 <42591438+pax217@users.noreply.github.com>
2020-01-01 17:31:40 -0600

    Add Audit
    Se agrega funcionalidad para la creacion de auditoria en el CRUD de Entidades

---
+++ src/Mahzan.Models/Enums/Audit/TableAuditEnum.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mahzan.Models.Enums.Audit
+{
+    public enum TableAuditEnum
+    {
+        MEMBERS,
+    }
+}
---
+++ src/Mahzan.Models/Migrations/20200101232730_Audits.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Mahzan.Models.Migrations
+{
+    public partial class Audits : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Audits",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    TableName = table.Column<string>(nullable: true),
+                    DateTime = table.Column<DateTime>(nullable: false),
+                    KeyValues = table.Column<string>(nullable: true),
+                    OldValues = table.Column<string>(nullable: true),
+                    NewValues = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Audits", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Grupos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Nombre = table.Column<string>(maxLength: 100, nullable: false),
+                    Activo = table.Column<bool>(nullable: false),
+                    MiembroId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Grupos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Audits");
+
+            migrationBuilder.DropTable(
+                name: "Grupos");
+        }
+    }
+}
