a40e947750d32c73e88e56241b39f96cb911e0f6
Carlos Maldonado <carlos.maldonado@conekta.com>
2020-01-09 13:48:26 -0600

    Commit

---
+++ src/Mahzan.DataAccess/Filters/_Base/FilterExpression.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+using Mahzan.Models.Enums.Expressions;
+
+namespace Mahzan.DataAccess.Filters._Base
+{
+    public class FilterExpression
+    {
+        public PropertyInfo PropertyInfo { get; set; }
+        public string PropertyName { get; set; }
+        public OperationsEnum Operator { get; set; }
+        public object Value { get; set; }
+    }
+}
