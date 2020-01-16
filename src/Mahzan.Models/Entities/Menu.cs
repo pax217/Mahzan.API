626c6894cd4b0af9b3b16e9e3e6a09db744dd319
pax217 <42591438+pax217@users.noreply.github.com>
2020-01-15 20:41:25 -0600

    Create Menu

---
+++ src/Mahzan.Models/Entities/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mahzan.Models.Entities
+{
+    public class Menu
+    {
+        public Guid RoleId { get; set; }
+
+        public Guid MenuItemId { get; set; }
+
+        public Guid MenuSubItemId { get; set; }
+
+    }
+}
