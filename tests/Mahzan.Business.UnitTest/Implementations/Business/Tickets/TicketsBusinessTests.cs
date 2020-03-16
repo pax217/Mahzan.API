0599c8d903545c271a35f54f2a7249758de8f2f0
Carlos Maldonado <carlos.maldonado@conekta.com>
2020-03-16 09:53:06 -0600

    Update TicketsBusinessTests.cs

--- tests/Mahzan.Business.UnitTest/Implementations/Business/Tickets/TicketsBusinessTests.cs
+++ tests/Mahzan.Business.UnitTest/Implementations/Business/Tickets/TicketsBusinessTests.cs
@@ -6,6 +6,12 @@ namespace Mahzan.Business.UnitTest.Implementations.Business.Tickets
 {
     public class TicketsBusinessTests
     {
+        public Guid StoresId = new Guid("00fd745c-080f-4618-8fae-bf2b6a2734bf");
+
+        public Guid PointsOfSalesId = new Guid("12ae3138-83f8-4527-9d45-4df23eda0828");
+
+        public Guid PaymentTypesId = new Guid("644f7e11-8c52-430b-8ca3-fa4c82e8d550");
+
         public TicketsBusinessTests()
         {
         }
