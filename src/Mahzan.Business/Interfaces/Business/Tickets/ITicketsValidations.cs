1b80db0e7b80f512f6d31c200ff55fe3c9761bfc
pax217 <42591438+pax217@users.noreply.github.com>
2020-03-26 12:01:27 -0600

    Add Tickets Validations

---
+++ src/Mahzan.Business/Interfaces/Business/Tickets/ITicketsValidations.cs
@@ -0,0 +1,27 @@
+﻿using Mahzan.Business.Interfaces.Validations.Tickets;
+using Mahzan.Business.Results.Tickets;
+using Mahzan.DataAccess.DTO.Tickets;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahzan.Business.Interfaces.Business.Tickets
+{
+    public class ITicketsValidations
+    {
+        public IAddTicketsValidations _addTicketsValidations { get; set; }
+
+        public ITicketsValidations(
+            IAddTicketsValidations ticketsValidations) 
+        {
+            _addTicketsValidations = ticketsValidations;
+        }
+
+        public async Task<PostTicketsResult> AddTicketValid(AddTicketsDto addTicketsDto) 
+        {
+            return await _addTicketsValidations
+                         .AddTicketValid(addTicketsDto);
+        }
+    }
+}
---
+++ src/Mahzan.Business/Interfaces/Validations/Tickets/IAddTicketsValidations.cs
@@ -0,0 +1,15 @@
+﻿using Mahzan.Business.Results.Tickets;
+using Mahzan.DataAccess.DTO.Tickets;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mahzan.Business.Interfaces.Validations.Tickets
+{
+    public interface IAddTicketsValidations
+    {
+        Task<PostTicketsResult> AddTicketValid(AddTicketsDto addTicketsDto);
+
+    }
+}
